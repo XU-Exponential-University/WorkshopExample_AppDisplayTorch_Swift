2e2903e8786958f0437c509aca54fa1fc6f28f33
Felix Prünte <f.pruente@xu-university.de>
2020-02-20 11:20:14 +0100

    initial push.

--- DisplayTorch/ViewController.swift
+++ DisplayTorch/ViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var onButton: UIButton!
+    let turnOnText = "tap here to switch on the torch"
+    let turnOffText = "tap here to switch off the torch"
+    
+    var isOn = false
+    
+    let onColor = UIColor.white
+    let offColor = UIColor.black
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        onButton.setTitle(turnOnText, for: .normal)
+        view.backgroundColor = offColor
     }
 
-
+    @IBAction func switchTorch(_ sender: UIButton) {
+        view.backgroundColor = isOn ? offColor : onColor
+        onButton.setTitle(isOn ? turnOnText : turnOffText, for: .normal)
+        
+        isOn = !isOn
+    }
+    
 }
 
